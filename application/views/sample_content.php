a446ec83f37b00017468121c9fd3169540815b83
Ryan <ryan@thingylab.com>
2012-02-01 23:59:03 -0600

    removed another reference to kickstart

--- application/views/sample_content.php
+++ application/views/sample_content.php
@@ -1,13 +1,13 @@
       <div class="content">
         <div class="page-header">
-          <h1>CI KickStart <small>CodeIgniter + Tank Auth + Bootstrap</small></h1>
+          <h1>CI Bootstrap <small>CodeIgniter + Tank Auth + Bootstrap</small></h1>
         </div>
         <div class="row">
           <div class="span10">
 
             <h2>QuickStart CodeIgniter</h2>
             <p>This project pulls together several popular components to provide a jumpstart when creating your next web application.</p>
-            <p>Read more at <a href="http://weeklybuild.com/2012/01/ci-kickstart-a-web-app-jump-start/">WeeklyBuild.com</a></p>
+            <p>Read more at <a href="http://weeklybuild.com/2012/01/ci-bootstrap-a-web-app-jump-start/">WeeklyBuild.com</a></p>
             <p></p>
             <h3>Getting Started</h3>
             <ol>
